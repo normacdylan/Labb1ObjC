297014d6c2ce4cf21127cb307403bebfb2edd97d
August Posner <august_posner@hotmail.com>
2018-01-16 12:42:20 +0100

    Initial Commit

---
+++ AugustPosnerLabb1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AugustPosnerLabb1
+//
+//  Created by August Posner on 2018-01-16.
+//  Copyright © 2018 August Posner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
